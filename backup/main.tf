ce854332b58344b01e6a09ad3abc4da98e5e503a
Alaris-support <brykinivan4@gmail.com>
2023-10-23 13:14:09 +0300

    Update main.tf

--- backup/main.tf
+++ backup/main.tf
@@ -17,12 +17,12 @@ resource "yandex_vpc_network" "central-1-network" {
   name = "network2"
 }
 
-resource "yandex_vpc_subnet" "subnet-2" {
-  name           = "subnet2"
-  zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.central-1-network.id
-  v4_cidr_blocks = ["192.168.100.0/24"]
-}
+#resource "yandex_vpc_subnet" "subnet-2" {
+#  name           = "subnet2"
+#  zone           = "ru-central1-a"
+#  network_id     = yandex_vpc_network.central-1-network.id
+#  v4_cidr_blocks = ["192.168.100.0/24"]
+#}
 
 resource "yandex_lb_network_load_balancer" "lb-1" {
   name  = "lb-1"
