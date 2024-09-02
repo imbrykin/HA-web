e6f54298ad084141e2a3a6eba6cd85a4af417a4b
imbrykin <brykinivan4@gmail.com>
2024-09-03 02:20:07 +0300

    adding test file

---
+++ terraform-files/meta-test.tf
@@ -0,0 +1,52 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_bastion
+}
+
+resource "yandex_vpc_network" "default" {
+  name = "default-network"
+}
+
+resource "yandex_vpc_subnet" "default" {
+  name           = "default-subnet"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.default.id
+  v4_cidr_blocks = ["10.0.0.0/24"]
+}
+
+resource "yandex_compute_instance" "test-vm" {
+  name        = "test-vm"
+  platform_id = "standard-v1"
+  zone        = "ru-central1-a"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.bastion_image_id
+      size     = var.bastion_disk_size
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.default.id
+    nat       = true
+  }
+
+  metadata = {
+    user-data = templatefile("./meta.yml", {})
+    serial-port-enable = "1"
+  }
+}
