d274a644d25a68975fbcad7881726f2dd7a13344
imbrykin <brykinivan4@gmail.com>
2024-09-04 22:29:11 +0300

    trying another meta file

--- terraform-files/meta-test.tf
+++ terraform-files/meta-test.tf
@@ -47,16 +47,7 @@ resource "yandex_compute_instance" "test-vm" {
   }
 
   metadata = {
-    user-data = <<-EOT
-      #cloud-config
-      users:
-        - name: user
-          groups: sudo
-          shell: /bin/bash
-          sudo: 'ALL=(ALL) NOPASSWD:ALL'
-      EOT
-    ssh-keys = "user:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCwUweG4AAWSkzixxuXdWaTsZv24jr/kBjFYrIhPe3mNrbLd8mC/eoEKEP3nhxNP+JLklvSr2YZUl2Ywh8/3xApW1e97p3qbC2AqRG17vMyPyzogLeSECRYzN8C+gO2kHi5OGXjnvjR4TjyZj2+a1fQYlylhURxOTNa8YgRC96hgT0Fn+yOJtxf7qMInVa6ZZ25OFCNCmRFo6WQdV2JkNlfHE7BmD1WGz1lHUPQlMob8z7nWbIDo1gYcFJjOj0ROjCPltIQFwR7ZTrvtujq1KtbCHbxFnycRS7LexmDGCDdzMRzMNotAxti3sAjIbHySh7KvgClUOV6cVqcXxGyR0kZ brykinivan@yandex.cloud"
+    user-data = file("terraform-files/meta.yml")
     serial_port_enable = "true"
   }
-
 }
