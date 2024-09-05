18a2caa991eb76d59f4b5e9a139d0eb0037ecd8d
imbrykin <brykinivan4@gmail.com>
2024-09-05 18:40:29 +0300

    adding web sub

---
+++ ansible-playbooks/web.tf
@@ -0,0 +1,111 @@
+
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_bastion
+}
+
+# New Web Internal Network and Subnets
+resource "yandex_vpc_network" "web_internal" {
+  name        = var.network_web_internal
+  description = "Internal web network"
+}
+
+resource "yandex_vpc_subnet" "web_internal_a" {
+  name           = var.subnet_web_internal_a
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.web_internal.id
+  v4_cidr_blocks = ["10.10.0.0/24"]
+}
+
+resource "yandex_vpc_subnet" "web_internal_b" {
+  name           = var.subnet_web_internal_b
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.web_internal.id
+  v4_cidr_blocks = ["10.11.0.0/24"]
+}
+
+# Security Group for Web Servers
+resource "yandex_vpc_security_group" "internal_web_sg" {
+  name        = var.sg_internal_web
+  network_id  = yandex_vpc_network.web_internal.id
+  description = "Internal security group for Web"
+
+  ingress {
+    description       = "SSH-in"
+    protocol          = "TCP"
+    port              = 22
+    predefined_target = "self_security_group"
+  }
+
+  ingress {
+    description       = "HTTP Healthcheck"
+    protocol          = "TCP"
+    from_port         = 80
+    to_port           = 80
+    predefined_target = "self_security_group"
+  }
+
+  egress {
+    description    = "All-out"
+    protocol       = "ANY"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# VM Instances for Web
+resource "yandex_compute_instance" "web0" {
+  name        = "web0"
+  platform_id = "standard-v1"
+  zone        = "ru-central1-a"
+  resources {
+    cores  = 2
+    memory = 4
+  }
+  boot_disk {
+    initialize_params {
+      image_id = var.web_vm_image_id
+      size     = var.web_vm_disk_size
+    }
+  }
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.web_internal_a.id
+    security_group_ids = [yandex_vpc_security_group.internal_web_sg.id]
+  }
+
+  metadata = {
+    ssh-keys = "user:${file(var.ssh_public_key)}"
+  }
+}
+
+resource "yandex_compute_instance" "web1" {
+  name        = "web1"
+  platform_id = "standard-v1"
+  zone        = "ru-central1-b"
+  resources {
+    cores  = 2
+    memory = 4
+  }
+  boot_disk {
+    initialize_params {
+      image_id = var.web_vm_image_id
+      size     = var.web_vm_disk_size
+    }
+  }
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.web_internal_b.id
+    security_group_ids = [yandex_vpc_security_group.internal_web_sg.id]
+  }
+
+  metadata = {
+    ssh-keys = "user:${file(var.ssh_public_key)}"
+  }
+}
--- terraform-files/web-tf/web.tf
+++ terraform-files/web-tf/web.tf
@@ -13,71 +13,23 @@ provider "yandex" {
   folder_id = var.folder_bastion
 }
 
-# New Web Internal Network and Subnets
-resource "yandex_vpc_network" "web_internal" {
-  name        = var.network_web_internal
-  description = "Internal web network"
-}
-
-resource "yandex_vpc_subnet" "web_internal_a" {
-  name           = var.subnet_web_internal_a
-  zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.web_internal.id
-  v4_cidr_blocks = ["10.10.0.0/24"]
-}
-
-resource "yandex_vpc_subnet" "web_internal_b" {
-  name           = var.subnet_web_internal_b
-  zone           = "ru-central1-b"
-  network_id     = yandex_vpc_network.web_internal.id
-  v4_cidr_blocks = ["10.11.0.0/24"]
-}
-
-# Security Group for Web Servers
-resource "yandex_vpc_security_group" "internal_web_sg" {
-  name        = var.sg_internal_web
-  network_id  = yandex_vpc_network.web_internal.id
-  description = "Internal security group for Web"
-
-  ingress {
-    description       = "SSH-in"
-    protocol          = "TCP"
-    port              = 22
-    predefined_target = "self_security_group"
-  }
-
-  ingress {
-    description       = "HTTP Healthcheck"
-    protocol          = "TCP"
-    from_port         = 80
-    to_port           = 80
-    predefined_target = "self_security_group"
-  }
-
-  egress {
-    description    = "All-out"
-    protocol       = "ANY"
-    v4_cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 # VM Instances for Web
-resource "yandex_compute_instance" "web0" {
-  name        = "web0"
-  platform_id = "standard-v1"
+resource "yandex_compute_instance" "web1" {
+  name        = "web1"
   zone        = "ru-central1-a"
   resources {
     cores  = 2
-    memory = 4
+    memory = 2
   }
   boot_disk {
     initialize_params {
       image_id = var.web_vm_image_id
       size     = var.web_vm_disk_size
+      type     = "network-ssd"
     }
   }
   network_interface {
-    subnet_id          = yandex_vpc_subnet.web_internal_a.id
+    subnet_id          = yandex_vpc_subnet.web_internal_b.id
     security_group_ids = [yandex_vpc_security_group.internal_web_sg.id]
   }
 
@@ -101,11 +53,21 @@ resource "yandex_compute_instance" "web1" {
     }
   }
   network_interface {
-    subnet_id          = yandex_vpc_subnet.web_internal_b.id
-    security_group_ids = [yandex_vpc_security_group.internal_web_sg.id]
+    subnet_id           = yandex_vpc_subnet.bastion_internal_a.id
+    nat                 = false
+    security_group_ids  = [yandex_vpc_security_group.internal_bastion_sg.id]
+    ip_address          = "172.16.0.254"
   }
 
+  network_interface {
+    subnet_id           = yandex_vpc_subnet.bastion_external_a.id
+    nat                 = false
+    security_group_ids  = [yandex_vpc_security_group.external_bastion_sg.id]
+    ip_address          = "172.16.1.254"
+  }  
+
   metadata = {
-    ssh-keys = "user:${file(var.ssh_public_key)}"
+    user-data = templatefile("./meta.yml", {})
+    serial-port-enable = "1"
   }
 }
