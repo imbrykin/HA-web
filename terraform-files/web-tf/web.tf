afe8ad14b1b689883bed7afc9df53d1c1021bdc0
imbrykin <brykinivan4@gmail.com>
2024-09-05 19:06:31 +0300

    adding vpc

--- terraform-files/web-tf/web.tf
+++ terraform-files/web-tf/web.tf
@@ -13,8 +13,108 @@ provider "yandex" {
   folder_id = var.folder_bastion
 }
 
+resource "yandex_vpc_network" "bastion_internal" {
+  name        = var.network_bastion_internal
+  description = "Internal bastion network"
+}
 
-# Existing configuration is kept
+resource "yandex_vpc_subnet" "bastion_internal_a" {
+  name           = var.subnet_bastion_internal_a
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.bastion_internal.id
+  v4_cidr_blocks = ["172.16.0.0/24"]
+}
+
+resource "yandex_vpc_subnet" "bastion_internal_b" {
+  name           = var.subnet_bastion_internal_b
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.bastion_internal.id
+  v4_cidr_blocks = ["172.17.0.0/24"]
+}
+
+resource "yandex_vpc_network" "bastion_external" {
+  name        = var.network_bastion_external
+  description = "External bastion network"
+}
+
+resource "yandex_vpc_subnet" "bastion_external_a" {
+  name           = var.subnet_bastion_external_a
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.bastion_external.id
+  v4_cidr_blocks = ["172.16.1.0/24"]
+}
+
+resource "yandex_vpc_security_group" "internal_bastion_sg" {
+  name        = var.sg_internal_bastion
+  network_id  = yandex_vpc_network.bastion_internal.id
+  description = "Internal security group for Bastion"
+
+  ingress {
+    description       = "SSH-in"
+    protocol          = "TCP"
+    port              = 22
+    predefined_target = "self_security_group"
+  }
+
+  ingress {
+    description    = "L7-80"
+    protocol       = "TCP"
+    from_port      = 80
+    to_port        = 80
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description    = "L7-443"
+    protocol       = "TCP"
+    from_port      = 443
+    to_port        = 443
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description    = "L7-30080"
+    protocol       = "TCP"
+    from_port      = 30080
+    to_port        = 30080
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description       = "ICMP allow"
+    protocol          = "ICMP"
+    predefined_target = "self_security_group"
+  }
+
+  egress {
+    description    = "All-out"
+    protocol       = "ANY"
+    from_port      = 0
+    to_port        = 65535
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "yandex_vpc_security_group" "external_bastion_sg" {
+  name        = var.sg_external_bastion
+  network_id  = yandex_vpc_network.bastion_external.id
+  description = "External security group for Bastion"
+
+  ingress {
+    description = "SSH-in"
+    protocol    = "TCP"
+    port       = 22
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "All-out"
+    protocol    = "ANY"
+    from_port   = 0
+    to_port     = 65535
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 # 1. DNS Zone Configuration
 resource "yandex_dns_zone" "internal_cloud" {
